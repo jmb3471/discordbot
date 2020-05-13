090de96a3ea852fb7f6102e86fa6c5cb1d73b2a8
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2020-05-12 20:38:34 -0400

    fixed error with list length

--- bot.py
+++ bot.py
@@ -27,36 +27,45 @@ async def on_message(message):
     if message.author == client.user:
         return
     parts = message.content.split()
-    if(parts[0] == "!horror"):
-        horrorMovies = MovieLists.ReadFromFile("GenreLists/horrorTitles.txt")
-        randomNum = random.randint(0, len(horrorMovies))
-        await message.channel.send(horrorMovies[randomNum])
-        await message.channel.send(horrorMovies[randomNum + 1])
-        await message.channel.send(horrorMovies[randomNum + 2])
-    elif(parts[0] == "!add"):
-        try:
-            print(parts[1] + "list.txt")
-            print("herro?")
+    try:
+        filename = parts[0]
+        filename = filename[1:]
+        mylist = MovieLists.ReadFromFile(filename)
+        randomNum = random.randint(0, len(mylist) - 1)
+        await message.channel.send(mylist[randomNum])
 
-            newMovie = ""
-            for i in range(2, len(parts)):
-                if(i < len(parts) - 1):
-                    newMovie += parts[i] + " "
-                else:
-                    newMovie += parts[i]
-            MovieLists.WriteToFile("Jonathanlist.txt", newMovie)
 
-        except IOError:
-            await message.channel.send("List doesn't exist")
+    except IOError:
+        if(parts[0] == "!horror"):
+            horrorMovies = MovieLists.ReadFromFile("GenreLists/horrorTitles.txt")
+            randomNum = random.randint(0, len(horrorMovies) - 1)
+            await message.channel.send(horrorMovies[randomNum])
+            await message.channel.send(horrorMovies[randomNum + 1])
+            await message.channel.send(horrorMovies[randomNum + 2])
+        elif(parts[0] == "!add"):
+            try:
+                print(parts[1] + "list.txt")
 
-    elif(parts[0] == "!create"):
-        writeFile = open(str(parts[1]) + "list.txt", "w")
-        writeFile.close()
-        print("Done")
+                newMovie = ""
+                for i in range(2, len(parts)):
+                    if(i < len(parts) - 1):
+                        newMovie += parts[i] + " "
+                    else:
+                        newMovie += parts[i]
+                MovieLists.WriteToFile(parts[1] + "list.txt", newMovie)
 
-    elif(parts[0] == "!addto"):
-        writeFile = open(str(parts[1]) + "list.txt", "w")
-        writeFile.write("\n" + parts[2])
-        writeFile.close()
+            except IOError:
+                await message.channel.send("List doesn't exist")
+
+        elif(parts[0] == "!create"):
+            writeFile = open(str(parts[1]) + "list.txt", "w")
+            writeFile.close()
+
+        elif(parts[0] == "!pick"):
+            writeFile = open(str(parts[1]) + "list.txt", "w")
+            writeFile.write("\n" + parts[2])
+            writeFile.close()
+
+
 
 client.run(auth.TOKEN)
